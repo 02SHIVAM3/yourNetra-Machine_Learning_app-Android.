7141352b592dffc89ec532dd231086d20c8fcb87
shivam <shivam@kali.test>
2024-05-24 04:30:34 +0530

    Implemeted the texture view and setted up the camera for object detection

---
+++ app/src/main/java/com/example/yournetra/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.yournetra
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.yournetra.databinding.ActivityMainBinding
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    private lateinit var cameraManager: CameraManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+
+        getPermission()
+
+        val handlerThread =HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        binding.textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun openCamera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0],object:CameraDevice.StateCallback(){
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+
+                val surfaceTexture = binding.textureView.surfaceTexture
+
+                val surface = Surface(surfaceTexture)
+
+                val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface),object: CameraCaptureSession.StateCallback(){
+
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        session.setRepeatingRequest(captureRequest.build(),null,null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        TODO("Not yet implemented")
+                    }
+                },handler)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                TODO("Not yet implemented")
+            }
+        },handler)
+    }
+
+
+    private fun getPermission() {
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA),101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermission()
+        }
+    }
+}
