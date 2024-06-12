49d1c2200fc2056fc18b8f95621383b00059199d
shivam <shivanshsharma8899@gmail.com>
2024-06-12 08:28:57 +0530

    commiting 8 changes after implementing the machine learning model using Tensorflow

---
+++ app/src/main/java/com/example/yournetra/Splashscreen.kt
@@ -0,0 +1,36 @@
+package com.example.yournetra
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.yournetra.databinding.ActivitySplashscreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class Splashscreen : AppCompatActivity() {
+
+    private val binding: ActivitySplashscreenBinding by lazy {
+        ActivitySplashscreenBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        } ,3000)
+
+
+    }
+}
